11c3a65fe626d07a6d10b30caf1174d13261a527
amzad3687 <k.amzad1706@gmail.com>
2024-02-25 02:30:10 +0530

    huma

--- src/main/java/com/info/test/Test.java
+++ src/main/java/com/info/test/Test.java
@@ -1,14 +1,8 @@
 package com.info.test;
 
-import java.util.logging.Logger;
-
 public class Test {
 
-	private static Logger logger = Logger.getLogger("SpringBootDemoApplication.class");
-
 	public static void main(String[] args) {
-
-		logger.info("executed main ...");
 
 		System.out.println(" Amjad khan");
 
