2751f18a9efea9442c0441cfa9ff1f8a2e9963ee
amzad3687 <k.amzad1706@gmail.com>
2024-02-25 16:40:41 +0530

    test class updated

--- src/main/java/com/info/test/Test.java
+++ src/main/java/com/info/test/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println(" Amjad khan");
-		System.out.println(" Huma khan");
+		System.out.println("tested complete");
+		
 
 	}
 
